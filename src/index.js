def6c30c96c9fa8c7d372d302930fd7b4c63f94e
alexandra.harko <sanyuta93@gmail.com>
2021-03-15 17:45:28 +0300

    feat: add typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
+  if (array && array.length > 0){
+    return array.reduce(function (p, v) {
+      return ( p < v ? p : v );
+    });
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  if (array && array.length > 0){
+    return array.reduce(function (p, v) {
+      return ( p > v ? p : v );
+    });
+  }
   return 0;
+  
 }
 
 exports.avg = function avg (array) {
+  if (array && array.length > 0){
+    return array.reduce((a, b) => a + b) / array.length;
+  }
   return 0;
 }
